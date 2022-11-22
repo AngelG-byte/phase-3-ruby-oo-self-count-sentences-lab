0fc3285e6c1935bb9c039efdede6be685a30603b
Angel Marte <robotking134@gmail.com>
2022-11-22 10:40:49 -0500

    commit tree 0ba78514ea568116acc40795a9b90c67c77af6a5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    
+attr_accessor :sentence
+  def initialize sentence
+    @sentence = sentence
+  end
+def sentence? 
+    self.end_with?('.')
   end
 
   def question?
-
+ self.end_with?('?')
   end
 
   def exclamation?
-
+ self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/['?.!']+/).count
   end
 end
